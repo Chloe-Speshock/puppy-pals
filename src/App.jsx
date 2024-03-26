132d3f95ccf32917f9a99912da0cab0b0c09d7bc
Chloe Speshock <cespeshock@gmail.com>
2024-03-26 11:47:59 -0500

    mapped over puppies

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import { puppyList } from "./data.js";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppyList);
+  return (
+    <>
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+    </>
+  );
 }
 
 export default App;

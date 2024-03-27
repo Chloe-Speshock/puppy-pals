ea8237dc3c94f3442b8a11ff94f580dd2248746a
Chloe Speshock <cespeshock@gmail.com>
2024-03-27 10:25:46 -0500

    added css styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,35 @@ import "./App.css";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
   console.log(puppyList);
+
+  function handleClick() {}
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
     <>
       {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
       })}
+      {featPupId && (
+        <div className="pupId">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </>
   );
 }
